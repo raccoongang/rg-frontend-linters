5373bb3bce176a97a667be530240eb021dcd540b
Peter Kulko <peter.kulko@raccoongang.com>
2026-08-04 13:11:53 +0300

    fix: allow devDependencies in import/no-extraneous-dependencies rule (#6)

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -1,4 +1,10 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { createConfig } = require('@openedx/frontend-build');
 
-module.exports = createConfig('eslint');
+module.exports = {
+  ...createConfig('eslint'),
+  rules: {
+    ...createConfig('eslint').rules,
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  },
+};
